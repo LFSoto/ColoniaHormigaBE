779f264a8db8e0ec26788be392a1b2befbca339f
Diego Cabezas <dcabezas@its.work>
2023-08-19 20:56:50 -0600

    [MOD] Mejoras en el unit testing

--- src/test/java/com/hormiguero/reina/ReinaApplicationTests.java
+++ src/test/java/com/hormiguero/reina/ReinaApplicationTests.java
@@ -1,140 +1,165 @@
 package com.hormiguero.reina;
 
+import java.util.List;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.Assert;
 
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.util.Assert;
 
 import com.hormiguero.reina.controller.HormigaController;
+import com.hormiguero.reina.entity.HormigaEntity;
 import com.hormiguero.reina.service.HormigueroUris;
 import com.hormiguero.reina.service.HormigueroUris.SubSistemas;
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
+@TestInstance(Lifecycle.PER_CLASS)
 public class ReinaApplicationTests {
 
 	@Autowired
 	public HormigaController instance;
 	
 	private MockMvc _mock;
-	
+	private ObjectMapper mapper;
+	private List<HormigaEntity> ants;
 	private boolean entornoStatus;
 	private boolean comidaStatus;
 	
-	@BeforeEach
+	@Test
+	@BeforeAll
 	public void initializeApp() {
 		System.setProperty("spring.data.mongodb.uri", "mongodb+srv://queen:reina2023reina@hormigareina.twxfm7c.mongodb.net/hormiguero?retryWrites=true&w=majority");
 		this._mock = standaloneSetup(instance)
 				.build();
+		
+		this.entornoStatus = true;
+		this.comidaStatus = true;
+		this.mapper = new ObjectMapper();
 	}
 	
+	@Test
+	@BeforeEach
+	public void testEntorno() {
+		Assert.isTrue(this.entornoStatus, "Imposible crear nuevas hormigas cuando el Subsistema de Entorno esta caido. Endpoint: " + HormigueroUris.getInstance().getUrl(SubSistemas.ENTORNO));
+	}
+	
+	@Test
+	@BeforeEach
+	public void testComida() {
+		Assert.isTrue(this.comidaStatus, "Imposible crear nuevas hormigas cuando el Subsistema de Comida esta caido. Endpoint: " + HormigueroUris.getInstance().getUrl(SubSistemas.COMIDA));
+	}
 
 	
 	@Test
 	@Order(0)
 	public void testFoodCost()  throws Exception {
-		try {
-			this._mock.perform(get("/v1/entorno/foodCost"))
-				.andExpect(MockMvcResultMatchers.status().isOk());
-			this.entornoStatus = true;
-		} catch (Exception ex) {
-			this.entornoStatus = false;
-			throw ex;
-		}
+		this.entornoStatus = false;
+		this._mock.perform(get("/v1/entorno/foodCost"))
+			.andExpect(MockMvcResultMatchers.status().isOk());
+		this.entornoStatus = true;
 	}
 	
 	@Test
 	@Order(1)
 	public void testFoodAvailable()  throws Exception {
-		try {
-			this._mock.perform(get("/v1/comida/foodAvailable"))
-				.andExpect(MockMvcResultMatchers.status().isOk());
-			this.comidaStatus = true;
-		} catch (Exception ex) {
-			this.comidaStatus = false;
-			throw ex;
-		}
+		this.comidaStatus = false;
+		this._mock.perform(get("/v1/comida/foodAvailable"))
+			.andExpect(MockMvcResultMatchers.status().isOk());
+		this.comidaStatus = true;
 	}
 
 	@Test
 	@Order(5)
 	public void testNuevaHormiga() throws Exception {
-		if ( !this.entornoStatus)
-			throw new Exception("Imposible crear nuevas hormigas cuando el Subsistema de Entorno esta caido. Endpoint: " + HormigueroUris.getInstance().getUrl(SubSistemas.ENTORNO));
-		if ( !this.comidaStatus)
-			throw new Exception("Imposible crear nuevas hormigas cuando el Subsistema de Comida esta caido. Endpoint: " + HormigueroUris.getInstance().getUrl(SubSistemas.COMIDA));
 		
-		String tipo = "COMPILE_TEST";
-		int[] values = {11,13};
-		String[] bodies = new String[values.length];
-		for (int i = 0; i < values.length; i++ ) {
-			ResultActions response = this._mock.perform(get("/v1/getHormiga?cantidad={0}&tipo={1}", values[i], tipo)
-					.accept(MediaType.APPLICATION_JSON))
-					.andExpect(MockMvcResultMatchers.status().isOk());
-			String body = response.andReturn().getResponse().getContentAsString();
-			Assert.hasLength(body, "La lista retornada por el servicio esta vacía");
-			
-			bodies[i] = body;
-		}
+		setAnts(getHormiga(10));
+	}
+	
+	@Test
+	@Order(6)
+	public void testNegociarHormiga() throws Exception {
 
-		for (String body : bodies) {
-			this._mock.perform(post("/v1/killHormiga").content(body).header("Content-Type", "application/JSON"))
-			.andExpect(MockMvcResultMatchers.status().isOk());	
-		}
+		List<HormigaEntity> negociated = getHormiga(1);
+		Assert.isTrue(negociated.addAll(getAnts()), "La nueva hormiga negociada no ha podido ser agregada a la lista existente.");
+		setAnts(negociated);
+	}
+
+	@Test
+	@Order(7)
+	public void testListtAll() throws Exception {
+		byte[] response = this._mock.perform(get("/v1/listAll"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andReturn().getResponse().getContentAsByteArray();
+		
+		List<HormigaEntity> all = mapper.readValue(response, new TypeReference<List<HormigaEntity>>() {});
+
+		Assert.isTrue(getAnts().size() == all.size(), "La lista total de hormigas devueltas (" + this.ants.size() + ") es diferente de las devueltas por el API (" + all.size() + ").");
 	}
 	
 	@Test
 	@Order(10)
 	public void testReleaseHormiga() throws Exception {
-		this._mock.perform(post("/v1/releaseHormiga").content(this.testHormigas("RELEASE")).header("Content-Type", "application/JSON"));
-		//.andExpect(MockMvcResultMatchers.status().isOk());
+		
+		this._mock.perform(post("/v1/releaseHormiga").content(mapper.writeValueAsBytes(getAnts().listIterator(5))).header("Content-Type", "application/JSON"))
+			.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+	
+	@Test
+	@Order(11)
+	public void testReasignacionHormiga() throws Exception {
+		List<HormigaEntity> reasigned = getHormiga(5);
+		Assert.isTrue(reasigned.size() == 5, "Se esperaba tomar 5 hormigas libres, pero se reasignaron apenas " + reasigned.size());
 	}
 	
 	@Test
 	@Order(20)
 	public void testKillHormiga() throws Exception {
-		this._mock.perform(post("/v1/killHormiga").content(this.testHormigas("DELETE")).header("Content-Type", "application/JSON"));
-		//.andExpect(MockMvcResultMatchers.status().isOk());
+		this._mock.perform(post("/v1/killHormiga").content(mapper.writeValueAsBytes(getAnts())).header("Content-Type", "application/JSON"))
+			.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 	
-	@Test
-	@Order(30)
-	public void testListtAll() throws Exception {
-		this._mock.perform(get("/v1/listAll"))
-		.andExpect(MockMvcResultMatchers.status().isOk());
-	}
 	
 	@Test
 	@Order(40)
 	public void testSwagger() throws Exception {
 		this._mock.perform(get("/swagger"))
-		.andExpect(MockMvcResultMatchers.status().isFound());
+			.andExpect(MockMvcResultMatchers.status().isFound());
 	}
 	
-	private String testHormigas(String tipo) throws Exception {
-		if ( !this.comidaStatus) {
-			throw new Exception("Subsistema de Comida esta caido");
-		}
-		if ( !this.entornoStatus) {
-			throw new Exception("Subsistema de Entorno esta caido");
-		}
-		ResultActions response = this._mock.perform(get("/v1/getHormiga?cantidad=15&tipo={0}", tipo).accept(MediaType.APPLICATION_JSON));
-		return response.andReturn().getResponse().getContentAsString();
+	public List<HormigaEntity> getAnts() {
+		return this.ants;
+	}
+	
+	public void setAnts(List<HormigaEntity> ants) {
+		this.ants = ants;
+	}
+	
+	private List<HormigaEntity> getHormiga(int cantidad) throws Exception {
+		
+		String url = "/v1/getHormiga?cantidad={0}&tipo=COMPILE_TEST";
+		String response = this._mock.perform(get(url, cantidad)).andReturn().getResponse().getContentAsString();
+		Assert.hasLength(response, "La respuesta de " + url + " esta vacía");
+		
+		List<HormigaEntity> ants = mapper.readValue(response, new TypeReference<List<HormigaEntity>>() {});
+		Assert.noNullElements(ants, "No se encontraron hormigas creadas");
+		Assert.isTrue(ants.size() == cantidad, "Se esperaba crear " + cantidad + " hormigas, pero se crearon apenas " + ants.size());
+		
+		return ants;
 	}
 }
