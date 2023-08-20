da386b20c57cdb12a5cfb90962c36b474cecead0
Diego Cabezas <dcabezas@its.work>
2023-08-19 23:32:39 -0600

    [DEL] Id de EntornoEntity ya que no se utiliza

--- src/main/java/com/hormiguero/reina/entity/EntornoEntity.java
+++ src/main/java/com/hormiguero/reina/entity/EntornoEntity.java
@@ -2,18 +2,12 @@ package com.hormiguero.reina.entity;
 
 public class EntornoEntity {
 
-	private String id;
 	private int antCost;
 	
 	public EntornoEntity() {
-		setId("");
 		setAntCost(0);
 	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
-	
+		
 	public void setAntCost(int antCost) {
 		this.antCost = antCost;
 	}
